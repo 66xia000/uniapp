40ebe27758b5ca3ca3ca2487d8775725a8b93e6e
light-charster <111579480+light-charster@users.noreply.github.com>
2024-07-17 19:09:43 +0800

    two

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,8 +1,8 @@
 <template>
     <view class="container mx-auto max-w-screen-lg shadow-lg rounded-lg p-6 h-20 mt-5" style="width: 80%;">
         <view class="flex items-center space-x-4">
-            <view>
-                <image src="../../static/image/head.png" class="rounded-full h-20 w-20"></image>
+            <view class="p-4 bg-white rounded-lg shadow-md w-80">
+                <img class="w-full h-40" src="https://cp1.douguo.com/upload/caiku/5/b/7/yuan_5b56cbf976015fc3fa60d293331e43e7.jpg" alt="Image Description">
             </view>
             <view class="ml-6">
                 <view>
