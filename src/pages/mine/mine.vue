e0bf2d74adaea583b1d17b74365b939378cd87bf
light-charster <111579480+light-charster@users.noreply.github.com>
2024-07-18 11:23:21 +0800

    submit

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -9,18 +9,19 @@
                     <uv-button type="primary" text="登录" class="avatar-wrapper w-40" open-type="chooseAvatar"
                         @chooseavatar="onChooseAvatar"></uv-button>
                 </view>
-
                 <view v-else class="ml-20">
-                    <input v-if="f" type="nickname" class="weui-input" placeholder="点击获取昵称" />
+                    <input v-if="f" @change="inp" type="nickname" class="weui-input" placeholder="点击获取昵称" />
+                    <view v-else class="text-2xl">{{ name }}</view>
                 </view>
             </view>
-
-
-
         </view>
     </view>
 
-
+    <view v-for="(item, index) in ll" :key="index"  style="display: flex; justify-content: center;">
+        <view class="mt-7" style="width: 90%;">
+            <uv-cell value=">" :label=item :center="true" customStyle="text-blue-500"></uv-cell>
+        </view>
+    </view>
 
 </template>
 
@@ -30,6 +31,8 @@ export default {
         return {
             flag: true,
             f: true,
+            name: '',
+            ll : ["抽奖","帮助","反馈","设置","退出登录"],
             avatarUrl: 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
         }
     },
@@ -37,6 +40,10 @@ export default {
         onChooseAvatar(res) {
             this.avatarUrl = res.avatarUrl
             this.flag = false;
+        },
+        inp(e) {
+            this.f = false;
+            this.name = e.detail.value;
         }
     }
 }
